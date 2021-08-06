61f67555252094c2a5a5fa4267e7430086b41354
Jeff Griffiths <jeff@eko-recordings.ca>
2021-08-06 13:49:41 -0700

    produces build error

---
+++ src/Auth.svelte
@@ -0,0 +1,14 @@
+<script>
+import * as wn from 'webnative';
+
+console.log(wn);
+
+</script>
+
+<div>This is Auth...</div>
+
+<!-- <script src="https://unpkg.com/webnative@0.26.0/dist/index.min.js"></script> -->
+
+<style>
+
+</style>
